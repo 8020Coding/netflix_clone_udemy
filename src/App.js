a3f82d089f4407f74d47f73f06f66bc3ae281089
Andrey Dovganyuk <andrewdovg@gmail.com>
2021-10-29 12:00:11 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './Master.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Header from './components/header/Header'
+import StoryCard1 from './components/story_card-1/StoryCard1'
 
 function App() {
   return (
     <>
       <Header />
+      <StoryCard1 />
     </>
   )
 }
---
+++ src/components/story_card-1/StoryCard1.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import './StoryCard1.css'
+
+const StoryCard1 = () => {
+  return (
+    <section className='d-flex container-fluid our-story-wrapper justify-content-center'>
+      <div className='text-wrapper justify-content-center d-flex flex-column'>
+        <h1 className='text-light fw-bolder dispaly-4 pb-2'>
+          Enjoy on your TV.
+        </h1>
+        <h5 className='text-light' style={{ fontSize: '1.6rem' }}>
+          Watch on Smart TVs, Playstation, Xbox,
+          <br /> Chromecast, Apple TV, Blu-ray players, and
+          <br /> more.
+        </h5>
+      </div>
+      <div>
+        <img
+          className='our-story-card-img'
+          src='https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/tv.png'
+          alt='tv'
+        />
+        <video
+          className='our-story-card-video'
+          src='https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-tv-0819.m4v'
+          muted
+          autoPlay
+          preload='auto'
+          loop
+        ></video>
+      </div>
+    </section>
+  )
+}
+
+export default StoryCard1
